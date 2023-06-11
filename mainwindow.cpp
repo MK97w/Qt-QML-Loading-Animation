f06b6e1edaa3e9e80d6b1b327fc4ca4d8c0ee900
MERT <kabukcuoglumert@gmail.com>
2023-06-12 00:14:23 +0300

    push button added

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,10 +6,28 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->quickWidget->setSource(QUrl("qrc:/loadingAnimation.qml"));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    static bool show{true};
+    if(show)
+    {
+        ui->quickWidget->show();
+        show = false;
+    }
+    else
+    {
+        ui->quickWidget->hide();
+        show = true;
+    }
+
 }
 
